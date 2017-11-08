2fe6a081611a599f99fe7dc1b9b38cca07e33606
oyid <904604082@qq.com>
2017-11-08 15:57:34 +0800

    修改 配置

--- src/main/java/com/oyid/Application.java
+++ src/main/java/com/oyid/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration  // service增加事务时，这里必须要增加这个注解
 @SpringBootApplication
 @MapperScan("com.oyid.mybatis.mapper")
 public class Application {
